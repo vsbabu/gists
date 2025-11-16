38db0b129aca0c224226d4ed4889f0b4a9db8bcb
Satheesh Vattekkat <vsbabu@gmail.com>
2025-11-16 19:49:24 +0530

    Added dark/light theme switcher

---
+++ switch_theme.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Quick script to switch between dark and light themese for editors I use
+# on LinuxMint.
+TO_DARK=false
+if [ $# -ge 1 ]; then
+  shopt -s nocasematch
+  if [ "$1" != "light" ]; then
+    TO_DARK=true
+  fi
+  shopt -u nocasematch
+else
+  current_hour=$(date +"%k")
+  if [[ "$current_hour" -ge 19 ]] || [[ "$current_hour" -lt 7 ]]; then
+    TO_DARK=true
+  fi
+fi
+# Switch between Catppuccin Frappe and Latte flavours. Assumption is that these
+# are already installed and the config files have one of it setup
+#  wezterm, lazyvim, doom-emacs, vscode, btop, desktop
+if $TO_DARK; then
+  sed -i "s/latte/frappe/g" ~/.config/wezterm/sv_common.lua
+  sed -i "s/latte/frappe/g" ~/.config/nvim/lua/plugins/colorscheme.lua
+  sed -i "s/latte/frappe/g" ~/.config/doom/config.el
+  sed -i "s/latte/frappe/g" ~/.config/Code/User/settings.json
+  sed -i "s/Latte/Frappé/g" ~/.config/Code/User/settings.json
+  sed -i "s/latte/frappe/g" ~/.config/btop/btop.conf
+  gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark-Aqua'
+else
+  sed -i "s/frappe/latte/g" ~/.config/wezterm/sv_common.lua
+  sed -i "s/frappe/latte/g" ~/.config/nvim/lua/plugins/colorscheme.lua
+  sed -i "s/frappe/latte/g" ~/.config/doom/config.el
+  sed -i "s/frappe/latte/g" ~/.config/Code/User/settings.json
+  sed -i "s/Frappé/Latte/g" ~/.config/Code/User/settings.json
+  sed -i "s/frappe/latte/g" ~/.config/btop/btop.conf
+  gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Aqua'
+fi
