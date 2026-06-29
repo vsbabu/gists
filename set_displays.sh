fdd36e6e6f5fd173cc64a93b79c52f2b61656947
SV <vsbabu@gmail.com>
2026-06-29 17:34:13 +0530

    Tweaked for X11 also

--- set_displays.sh
+++ set_displays.sh
@@ -3,26 +3,59 @@
 # For sway, when I connect an external monitor, running this script will arrange the displays into monitors.
 #  I usually keep monitor above laptop; or to the left of laptop (see below to change the script for this)
 # When lid is closed, internal display is turned off
-# Initial version used to be for i3/xrandr. To get this to work with xrandr, some work is needed.
 #
 #default sleep for 0 second if arg is not passed
 # I use this at times with a sleep of 5 seconds to issue
 # the command and then close the laptop screen
 sleep ${1:-0}
 
-# NOT really maintaining xrandr version
-CMD_RANDR=xrandr
-QRY_RANDR="xrandr -q"
-SCALE_RANDR="0.60x0.60"
+depencency_checks() {
+  local missing=()
+  for cmd in "$@"; do
+    if command -v ${cmd} &>/dev/null; then
+      :
+    else
+      missing+=("$cmd")
+    fi
+  done
+  if ((${#missing[@]} > 0)); then
+    echo "Please install following dependencies first:"
+    for i in "${missing[@]}"; do
+      echo "    $i"
+    done
+    exit 1
+  fi
+}
+
 if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+  depencency_checks wlr-randr jq
   CMD_RANDR=wlr-randr
-  QRY_RANDR=wlr-randr
   SCALE_RANDR="1.35" #1.35
+else
+  depencency_checks xrandr jq jc
+  CMD_RANDR=xrandr
+  SCALE_RANDR="0.60x0.60"
 fi
 
-#TODO: xrandr based monitor listing
-readarray -t MONITORS < <($QRY_RANDR | egrep "^\w+-" | cut -f1 -d' ' | sort)
-#ASSumption: first one in sorted out is laptop builtin monitor
+list_monitors() {
+  #ASSumption: first one in sorted out is laptop builtin monitor
+  if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+    wlr-randr --json | jq -r ".[].name" | sort
+  else
+    xrandr -q | jc xrandr | jq -r ".screens[].devices[].device_name" | sort
+  fi
+}
+get_external_monitor_shift() {
+  local monitor_name="$1"
+  local dimension="$2"
+  if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+    wlr-randr --json | jq ".[]|select(.name == \"${MONITORS[$i]}\")|.modes[]|select(.current)|.${dimension}"
+  else
+    xrandr -q | jc xrandr | jq ".screens[].devices[]|select(.device_name == \"${MONITORS[$i]}\")|.resolution_modes[]|select(.frequencies[].is_current)|.resolution_${dimension}"
+  fi
+}
+
+readarray -t MONITORS < <(list_monitors)
 
 # turn off internal display when lid is closed; on otherwise
 grep -ic open /proc/acpi/button/lid/LID0/state >/dev/null
@@ -41,10 +74,9 @@ for i in "${!MONITORS[@]}"; do
     $CMD_RANDR --output "${MONITORS[$i]}" --on
   fi
   p=$((i - 1))
-  #TODO: xrandr based monitor resolution fetch
   # find current monitor's resolution, push previous monitor down by that much height
   # for left side stacking, use width and $SHIFT,0
-  SHIFT=$(wlr-randr --json | jq ".[]|select(.name == \"${MONITORS[$i]}\")|.modes[]|select(.current)|.height")
+  SHIFT=$(get_external_monitor_shift "${MONITORS[$i]}" height)
   PREV_POSITION_FROM_CURRENT="--pos 0,$SHIFT"
   $CMD_RANDR --output "${MONITORS[$p]}" $PREV_POSITION_FROM_CURRENT "${MONITORS[$i]}"
 done
